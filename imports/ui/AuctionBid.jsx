3b64a7d591b4e20b57a83d5caa1a86120080b035
jojamber <64601382+jojamber@users.noreply.github.com>
2026-09-24 13:55:27 +0200

    add bid history

--- imports/ui/AuctionBid.jsx
+++ imports/ui/AuctionBid.jsx
@@ -1,5 +1,35 @@
+import { useSubscribe, useTracker } from "meteor/react-meteor-data";
+import { BidsCollection } from "/imports/api/BidsCollection";
+
 export const AuctionBid = ( { auctionId }) => {
+    const isLoading = useSubscribe("auctionBids", auctionId);
+
+    const bids = useTracker(() => {
+        return BidsCollection.find({ auctionId: auctionId }).fetch();
+    });
+
+    if (isLoading()) {
+        return <p aria-busy="true">Loading last bids...</p>;
+    }
+
     return (
-        <article className="auction-bid"></article>
-    )
+      <article className="auction-bid">
+        <small>
+          <strong>Last Bids for this auction:</strong>
+        </small>
+        {bids.length === 0 ? (
+          <p>No bids yet. </p>
+        ) : (
+          <ul className="bid-history-list">
+            {bids.map((bid) => (
+              <li key={bid._id} className="bid-history-item">
+                <span className="bid-time">{new Date(bid.createdAt).toLocaleTimeString()}</span>
+                <span className="bid-name">{bid.bidderName}</span>
+                <span className="bid-amount">{bid.amount} €</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </article>
+    );
 }
--- server/main.js
+++ server/main.js
@@ -4,13 +4,17 @@ import { BidsCollection } from "/imports/api/BidsCollection";
 import { check } from "meteor/check";
 
 async function insertAuction({ title, description, imageUrl, startingPrice, currentPrice, endTime }) {
-  await AuctionsCollection.insertAsync({ title, description, imageUrl, startingPrice, currentPrice, endTime });
+  return await AuctionsCollection.insertAsync({ title, description, imageUrl, startingPrice, currentPrice, endTime });
+}
+
+async function insertBid({ auctionId, bidderName, amount, createdAt = new Date() }) {
+  return await BidsCollection.insertAsync({ auctionId, bidderName, amount, createdAt });
 }
 
 Meteor.startup(async () => {
-  // Prefill AuctionsCollection if empty
+  // Prefill Collections if empty
   if ((await AuctionsCollection.find().countAsync()) === 0) {
-    await insertAuction({
+    const camera_auction_id = await insertAuction({
       title: "Vintage Camera",
       description: "A vintage camera from the 1950s.",
       imageUrl:
@@ -20,7 +24,7 @@ Meteor.startup(async () => {
       endTime: Date.now() + 4 * 60 * 60 * 1000, // 4h
     });
 
-    await insertAuction({
+    const vase_auction_id = await insertAuction({
       title: "Antique Vase",
       description: "An antique vase from the Ming dynasty.",
       imageUrl:
@@ -30,7 +34,7 @@ Meteor.startup(async () => {
       endTime: Date.now() + 6 * 60 * 60 * 1000, // 6h
     });
 
-    await insertAuction({
+    const bible_auction_id = await insertAuction({
       title: "Rare Bible",
       description: "A rare first edition bible from the 1600s.",
       imageUrl:
@@ -40,7 +44,7 @@ Meteor.startup(async () => {
       endTime: Date.now() + 2 * 60 * 60 * 1000, // 2h
     });
 
-    await insertAuction({
+    const car_auction_id = await insertAuction({
       title: "Mercedes-Benz 280 CE",
       description: "A classic Mercedes-Benz 280 CE from the 1980s. Perfect condion and well-maintained. A true collector's item for car enthusiasts. Don't miss the chance to own this iconic vehicle.",
       imageUrl:
@@ -48,6 +52,54 @@ Meteor.startup(async () => {
       startingPrice: 20000,
       currentPrice: 20000,
       endTime: Date.now() + 8 * 60 * 60 * 1000, // 8h
+    });
+  }
+
+  if ((await BidsCollection.find().countAsync()) === 0) {
+    const camera_auction = await AuctionsCollection.findOneAsync({ title: "Vintage Camera" });
+    const vase_auction = await AuctionsCollection.findOneAsync({ title: "Antique Vase" });
+
+    // Prefill Collections with bids
+    await insertBid({
+      auctionId: camera_auction._id,
+      bidderName: "Alice",
+      amount: 850,
+      createdAt: new Date(Date.now() - 30 * 60 * 1000),
+    });
+
+    await insertBid({
+      auctionId: camera_auction._id,
+      bidderName: "Bob",
+      amount: 900,
+      createdAt: new Date(Date.now() - 20 * 60 * 1000),
+    });
+
+    await insertBid({
+      auctionId: camera_auction._id,
+      bidderName: "Charlie",
+      amount: 950,
+      createdAt: new Date(Date.now() - 10 * 60 * 1000),
+    });
+
+    await insertBid({
+      auctionId: camera_auction._id,
+      bidderName: "Alice",
+      amount: 1500,
+      createdAt: new Date(Date.now() - 5 * 60 * 1000),
+    });
+
+    await insertBid({
+      auctionId: vase_auction._id,
+      bidderName: "David",
+      amount: 4600,
+      createdAt: new Date(Date.now() - 15 * 60 * 1000),
+    });
+
+    await insertBid({
+      auctionId: vase_auction._id,
+      bidderName: "Eve",
+      amount: 4700,
+      createdAt: new Date(Date.now() - 5 * 60 * 1000),
     });
   }
 
@@ -62,7 +114,7 @@ Meteor.startup(async () => {
 
   Meteor.publish("auctionBids", function (auctionId) {
     check(auctionId, String);
-    return BidsCollection.find({ _id: auctionId }, { sort: { createdAt: -1 } });
+    return BidsCollection.find({ auctionId: auctionId }, { sort: { createdAt: -1 } });
   });
 });
 
